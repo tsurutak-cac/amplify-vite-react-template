fbfe217548066a7f5057be887f498941593a8ea7
tsurutak <tsurutak@cac.co.jp>
2025-02-27 18:45:13 +0900

    lambda python実装まで

--- amplify/functions/lakeformation/index.py
+++ amplify/functions/lakeformation/index.py
@@ -1,9 +1,15 @@
 import json
 
+
 def handler(event, context):
-  return {
-      "statusCode": 200,
-      "body": json.dumps({
-          "message": "Hello World",
-      }),
-  }
+    return {
+        "statusCode": 200,
+        # Modify the CORS settings below to match your specific requirements
+        "headers": {
+            "Access-Control-Allow-Origin": "*",  # Restrict this to domains you trust
+            "Access-Control-Allow-Headers": "*",  # Specify only the headers you need to allow
+        },
+        "body": json.dumps({
+            "message": "Hello World",
+        }),
+    }
